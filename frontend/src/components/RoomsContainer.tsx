aad67652f06de4215e3c71d9ce160906d049faa1
clem <clementeahuatl@gmail.com>
2026-04-05 23:05:05 -0400

    Edit UI: Increase Rooms container width to fit the scroll bar

--- frontend/src/components/RoomsContainer.tsx
+++ frontend/src/components/RoomsContainer.tsx
@@ -39,7 +39,7 @@ export default function RoomsContainer(){
     }
     
     return(
-        <div className="border border-gray-500 w-132 h-75 rounded-2xl">
+        <div className="border border-gray-500 w-140 h-75 rounded-2xl">
             <div>
                 <div className="pl-2 mb-2 border-b flex flex-row gap-4">
                     <h1>Rooms</h1>
@@ -47,7 +47,7 @@ export default function RoomsContainer(){
                     <h1 className="w-30">Occupied: <span className="">{occuppiedRooms}</span></h1>
                     <h1 className="w-30">Cleaning: <span className="">{cleaningRooms}</span></h1>
                 </div>
-                <div className="flex flex-col gap-2 w-132 pl-2 h-65 overflow-y-auto">
+                <div className="flex flex-col gap-2.5 w-138 pl-2 h-65 overflow-y-auto">
                     {rooms.map((room)=>(
                         //@ts-ignore
                         <RoomContainer floor={room.floor} room_num={room.room_number} room_type={room.room_type} status={room.status}/>
