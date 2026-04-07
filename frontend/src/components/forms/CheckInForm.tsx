a318621e39e765fe82e774e2b869a24d8897558b
clem <clementeahuatl@gmail.com>
2026-04-07 19:49:36 -0400

    Only show unoccupied rooms in the check in form

--- frontend/src/components/forms/CheckInForm.tsx
+++ frontend/src/components/forms/CheckInForm.tsx
@@ -2,32 +2,42 @@ import SummitBtn from "../buttons/SubmitBtn";
 import instance from "../../utils/axios";
 import { useEffect, useState } from "react";
 export default function CheckInForm(){
-
+    
     const [rooms,setRooms] = useState([]);
-
+    let unoccupiedRooms:any[] = [];
 
     useEffect(()=>{
         try{
-            instance.get("/api/rooms").then((res)=>{
+            instance.get("/api/rooms")
+            .then((res)=>{
                 setRooms(res.data.results);
-            })
+            });
         }catch(error){
-
+            
         }
     },[])
 
+    const filterRooms = (room_type:string,room_list:any[]) =>{
+         for(let i = 0;i<=rooms.length-1;i++){
+            //@ts-ignore
+            if(rooms[i].status == room_type){
+                room_list.push(rooms[i])
+            }
+        }
+    }
 
+    filterRooms("Unoccupied",unoccupiedRooms);
     return (
-        <div className="flex flex-col gap-5 bg-gray-600 w-100 h-60  text-white rounded-2xl">
+        <div className="flex flex-col gap-5 bg-gray-600 w-120 h-60  text-white rounded-2xl">
             <span className="abolute text-center">Check In</span>
            <div className="flex flex-col gap-2 ml-5">
                 <label>Name</label>
                 <input placeholder="First Name" className="w-50 bg-gray-500 rounded-2xl pl-3"/>
            </div>
-            <select id="id" name="rooms" size={1} className="w-70 ml-5 bg-gray-500 overflow-y-auto">
-                {rooms.map((room)=>(
+            <select id="id" name="rooms" size={1} className="w-md ml-5 bg-gray-500 overflow-y-auto">
+                {unoccupiedRooms.map((unoccupiedRoom)=>(
                     //@ts-ignore
-                    <option value={room.id}>Floor:{room.floor} room number:{room.room_number} room type:{room.room_type} status:{room.status}</option>
+                    <option value={unoccupiedRoom.id}>Floor:{unoccupiedRoom.floor} room number:{unoccupiedRoom.room_number} room type:{unoccupiedRoom.room_type} status:{unoccupiedRoom.status}</option>
                 ))}
             </select>
             <div className="flex items-center justify-center">
